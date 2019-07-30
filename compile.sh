f67f5175692957234c9a1f4afb3e2d2dd17c86c6
zhanglu <zhanglu@atman360.com>
2019-07-30 19:57:18 +0800

    compile config

--- compile.sh
+++ compile.sh
@@ -4,7 +4,8 @@ runDir="webanno-webapp/src/main/docker"
 package="webanno-webapp/target/webanno-standalone-3.6.0-SNAPSHOT.jar"
 
 mvn clean install -Drat.skip=true
-cp ${package} ${runDir}
+cp -rf ${package} ${runDir}
 cd ${runDir}
+docker image build -t lusoftware/webanno .
 docker-compose up -d
 cd ${current}
